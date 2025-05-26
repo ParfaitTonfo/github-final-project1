c376281605d291a210e98a24288d6ae453645b1d
ParfaitTonfo <parfaittonfo@gmail.com>
2025-05-26 07:27:48 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+  #!/bin/bash
    # Ce script calcule l'intérêt simple donné le principal,
    # le taux d'intérêt annuel et la période de temps en années.
    # Ne pas utiliser ceci en production. À des fins d'exemple uniquement.
